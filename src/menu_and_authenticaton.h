9aad48861ac04a934b515559c467030b564f6ded
Vít Smolík <vit.smolik2@gmail.com>
2024-01-21 21:21:22 +0100

    started adding the function for starting/stopping the server, the call to the menuReturn() is failing from the start/stop func, logginng off for the day

--- src/menu_and_authenticaton.h
+++ src/menu_and_authenticaton.h
@@ -7,18 +7,19 @@
 #include <memory>
 #include "http_request.h"
 #include "get_server_info.h"
+#include "change_status.h"
 // why the fuck did I code this to be in one header file, fuck me
 
 std::string authToken;
-std::string token;
+extern std::string token;
 void reset();
 void getAuthKey(std::string id, std::string secret);
 void menuReturn();
 std::string serverIP;
 void getAuthKeyParams();
-std::string id;
+extern std::string id;
 std::string secret;
-std::string ip;
+extern std::string ip;
 
 void menu() {
     char opt;
@@ -53,8 +54,7 @@ void menu() {
             getServerInfo(serverIP, token);
             break;
         case '4':
-            std::cout << "Working on it!";
-            exit(0);
+            getStatusInfo(ip, token);
         case '5':
             exit(0);
         default:
@@ -119,7 +119,13 @@ void getAuthKey(std::string id, std::string secret, std::string ip) {
 
 void menuReturn() {
     void getAuthKey(std::string id, std::string secret, std::string ip);
+
     std::cout << "\nGetting you a fresh new token!\n";
+
+    std::cout << "\n" << id;
+    std::cout << "\n" << secret;
+    std::cout << "\n" << ip;
+
     getAuthKey(id, secret, ip);
     menu();
 }
