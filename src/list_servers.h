9892fc5f05603baa8e49d3d39135acad04ada4ac
Vít Smolík <vit.smolik2@gmail.com>
2024-01-22 15:03:39 +0100

    just started the .h file, need to log off

---
+++ src/list_servers.h
@@ -0,0 +1,19 @@
+//
+// Created by smajl on 22.01.2024.
+//
+
+#ifndef PUFFERSTARTER_CLI_LIST_SERVERS_H
+#define PUFFERSTARTER_CLI_LIST_SERVERS_H
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include "menu_and_authenticaton.h"
+
+// I hate my life
+
+void listAllServers() {
+    std::cout << "implement this or idk";
+}
+
+#endif //PUFFERSTARTER_CLI_LIST_SERVERS_H
