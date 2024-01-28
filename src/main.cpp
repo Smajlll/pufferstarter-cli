a6bf0b903c399a50a25106df9dd75d2588dc3254
Vít Smolík <vit.smolik2@gmail.com>
2024-01-28 13:15:54 +0100

    reworking it lol

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <boost/program_options.hpp>
 #include "auth_menu.h"
-namespace po = boost::program_options;
+#include "../libs/cxxopts.hpp"
 
 void ascii() {
     std::cout << R"(
@@ -35,74 +34,45 @@ void interactive(std::string location) {
     authMenu(location);
 }
 
-bool isValidStatus(const std::string& status) {
-    std::set<std::string> validStatusValues = {"off", "start", "kill"};
-    return validStatusValues.count(status) > 0;
-}
+int main(int argc, char* argv[]) {
+    std::string location;
 
-
-int main(int argc, const char *argv[]) {
-    try {
-        std::string location;
-
-        #ifdef _WIN32
-            location = std::getenv("USERPROFILE");
-            location += "\\.pufferstarter\\config.conf";
-        #else
-            location = std::getenv("HOME");
+    #ifdef _WIN32
+        location = std::getenv("USERPROFILE");
+        location += "\\.pufferstarter\\config.conf";
+    #else
+        location = std::getenv("HOME");
             location += "/.config/pufferstarter.conf";
-        #endif
+    #endif
 
 
-        po::options_description gen("Generic options");
-        gen.add_options()
-                ("help,?", "Print this message")
-                ("interactive,i", "Start the interactive mode");
-        po::options_description conf("Configuration options");
-        conf.add_options()
-                ("id", po::value<std::string>()->value_name("SERVER ID")->notifier([](const std::string& value){
-                    if (value.length() != 8) {
-                        throw std::invalid_argument("ID must be 8 characters long.");
-                    }
-                }), "Set the Server ID (length: 8)")
-                ("config,c", po::value<std::string>()->required()->value_name("PATH"),"Set a custom path to your Config File, rather then use the default.")
-                ("createConfig,r", "Creates the config at the default location");
-        po::options_description desc("Allowed options");
-        desc.add_options()
-                ("setStatus,s", po::value<std::string>()->value_name("STATUS")->notifier([](const std::string& status) {
-                    if (!isValidStatus(status)) {
-                        throw std::invalid_argument("Invalid value for --status");
-                    }
-                }), "Set status must be one off these values: \noff \nstart \nkill")
-                ("listAll,l", "Lists all servers and IDs")
-                ("getInfo,g","Gets the info about a server (requires --id)");
+    cxxopts::Options options("PufferStarter", " - command line options");
+    options.add_options("Generic options")
+            ("h,help", "Print this message")
+            ("i,interactive", "Start the interactive mode");
+
+    options.add_options("Configuration options")
+            ("id", "Set the Server ID (length: 8)",cxxopts::value<std::string>(), "<Server ID>")
+            ("c,config", "Set a custom path to your Config File, rather then use the default.", cxxopts::value<std::string>(), "<filename>")
+            ("r,createConfig", "Creates the config at the default location");
+
+    options.add_options("Functions")
+            ("s,setStatus","Set the status of the server\nRequires --id", cxxopts::value<std::string>(), "<start, stop, kill>")
+            ("l,listAll", "Lists all servers and IDs")
+            ("g,getInfo","Gets the info about a server\nRequires --id");
+
+    auto result = options.parse(argc, argv);
 
 
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
+    if (argc <= 2 || result.count("help")) {
+        std::cout << "Section 1:\n" << options.help() << "\n\n";
+        // std::cout << "Section 2:\n" << options.help("Configuration options") << "\n\n";
+        // std::cout << "Section 1:\n" << options.help('Functions') << std::endl;
+        return 0;
+    }
 
-        if (vm.count("interactive")) {
-            interactive(location);
-            return 0;
-        }
-
-        if (vm.count("help") || vm.count("?") || vm.empty()) {
-            std::cout << "Usage: pufferstarter_cli [options]\n";
-            std::cout << "You NEED a config file to use options, if you (for some reason) do not want to create it, you need to run PufferStarter interactively by using \"-i\"\n";
-            std::cout << gen << std::endl << conf << std::endl << desc << std::endl;
-        }
-
-        if (vm.count("listAll") || vm.count("l")) {
-            if (vm.count("config") || vm.count("c")) {
-                location = vm["config"].as<std::string>();
-            }
-            exit(0);
-
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+    if (result.count("interactive")) {
+        interactive(location);
     }
 
     return 0;
